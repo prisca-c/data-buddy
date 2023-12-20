01b0f3b8129451b9dd2a6c74ba895b1ed7e8a787
Prisca C <prisca.castanier@gmail.com>
2023-12-20 16:40:32 +0100

    refactor: DataBuddy's interface inside a single file

--- src/data_buddy.ts
+++ src/data_buddy.ts
@@ -1,54 +1,18 @@
 import * as fs from 'node:fs/promises'
 import { DataBuddyUtils } from './data_buddy_utils.js'
-
-type ReturnData = Object | null
-
-/**
- * Interface for the DataBuddy class.
- * @interface
- */
-export interface IDataBuddy {
-  /**
-   * Reads a file and returns its content.
-   * @param {string} path - The path to the file.
-   * @param {string} filename - The name of the file.
-   * @returns {Promise<ReturnData>} The content of the file or null if the file does not exist.
-   */
-  read(path: string, filename: string): Promise<ReturnData>
-
-  /**
-   * Creates a new file with the given data.
-   * @param {string} path - The path to the file.
-   * @param {string} filename - The name of the file.
-   * @param {object} data - The data to write to the file.
-   * @returns {Promise<ReturnData>} The content of the new file or an error if the file already exists.
-   */
-  create(path: string, filename: string, data: object): Promise<ReturnData>
-
-  /**
-   * Updates an existing file with the given data.
-   * @param {string} path - The path to the file.
-   * @param {string} filename - The name of the file.
-   * @param {object} data - The data to write to the file.
-   * @returns {Promise<ReturnData>} The content of the updated file or an error if the file does not exist.
-   */
-  update(path: string, filename: string, data: object): Promise<ReturnData>
-
-  /**
-   * Deletes a file.
-   * @param {string} path - The path to the file.
-   * @param {string} filename - The name of the file.
-   * @returns {Promise<boolean>} True if the file was deleted successfully, or an error if the file does not exist.
-   */
-  delete(path: string, filename: string): Promise<boolean>
-}
+import type {
+  DataBuddyInterface,
+  BaseParams,
+  ReturnData,
+  UpsertParams,
+} from './types/DataBuddyInterface.js'
 
 /**
  * Class implementing the IDataBuddy interface.
  * @class
  */
-export class DataBuddy extends DataBuddyUtils implements IDataBuddy {
-  async read(path: string, filename: string): Promise<ReturnData> {
+export class DataBuddy extends DataBuddyUtils implements DataBuddyInterface {
+  async read({ path, filename }: BaseParams): Promise<ReturnData> {
     try {
       this.validatePathAndFilename(path, filename)
       const data = await fs.readFile(`${path}/${filename}.json`, 'utf-8')
@@ -58,32 +22,32 @@ export class DataBuddy extends DataBuddyUtils implements IDataBuddy {
     }
   }
 
-  async create(path: string, filename: string, data: object): Promise<ReturnData> {
+  async create({ path, filename, data }: UpsertParams): Promise<ReturnData> {
     this.validatePathAndFilename(path, filename)
     const sanitizedData = this.sanitizeData(data)
 
-    if (await this.read(path, filename)) {
+    if (await this.read({ path, filename })) {
       throw new Error(`File ${filename} already exists in ${path}`)
     }
     await fs.writeFile(`${path}/${filename}.json`, JSON.stringify(sanitizedData))
-    return this.read(path, filename)
+    return this.read({ path, filename })
   }
 
-  async update(path: string, filename: string, data: object): Promise<ReturnData> {
+  async update({ path, filename, data }: UpsertParams): Promise<ReturnData> {
     this.validatePathAndFilename(path, filename)
     const sanitizedData = this.sanitizeData(data)
 
-    if (!(await this.read(path, filename))) {
+    if (!(await this.read({ path, filename }))) {
       throw new Error(`File ${filename} does not exist in ${path}`)
     }
     await fs.writeFile(`${path}/${filename}.json`, JSON.stringify(sanitizedData))
-    return this.read(path, filename)
+    return this.read({ path, filename })
   }
 
-  async delete(path: string, filename: string): Promise<boolean> {
+  async delete({ path, filename }: BaseParams): Promise<boolean> {
     this.validatePathAndFilename(path, filename)
 
-    if (!(await this.read(path, filename))) {
+    if (!(await this.read({ path, filename }))) {
       throw new Error(`File ${filename} does not exist in ${path}`)
     }
     await fs.unlink(`${path}/${filename}.json`)
