3729cafa31fb5ef84394511944f9922173d00bc1
Prisca C <prisca.castanier@gmail.com>
2023-12-20 16:41:12 +0100

    feat(validation): update path validation to throw a specific error

--- src/data_buddy_utils.ts
+++ src/data_buddy_utils.ts
@@ -6,7 +6,31 @@ export class DataBuddyUtils {
   }
 
   protected isValidPath(path: string): boolean {
-    return /^[a-zA-Z0-9_.-]*$/.test(path)
+    if (!/^[a-zA-Z0-9/]*$/.test(path)) {
+      throw new Error('Invalid characters in path')
+    }
+
+    if (path.startsWith('/')) {
+      throw new Error('Path cannot start with a slash')
+    }
+
+    if (path.endsWith('/')) {
+      throw new Error('Path cannot end with a slash')
+    }
+
+    if (path.includes('//')) {
+      throw new Error('Path cannot contain consecutive slashes')
+    }
+
+    if (path.includes(' ')) {
+      throw new Error('Path cannot contain spaces')
+    }
+
+    if (path.includes('..')) {
+      throw new Error('Path cannot contain consecutive dots')
+    }
+
+    return true
   }
 
   protected isValidFilename(filename: string): boolean {
