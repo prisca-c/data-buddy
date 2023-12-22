2282b3705d3eca9d05dfd074653b4484e00822d1
Prisca C <prisca.castanier@gmail.com>
2023-12-22 19:42:14 +0100

    feat(cache): add has and all method to cache tests

--- tests/cache.spec.ts
+++ tests/cache.spec.ts
@@ -54,4 +54,26 @@ test.group('Cache class', (group) => {
     const cacheValue = cache.get(key)
     assert.isUndefined(cacheValue)
   })
+
+  test('has() should return true if key exists', async ({ assert }) => {
+    cache.set(key, value)
+    const cacheValue = cache.has(key)
+    assert.isTrue(cacheValue)
+  })
+
+  test('has() should return false if key does not exist', async ({ assert }) => {
+    const cacheValue = cache.has(key)
+    assert.isFalse(cacheValue)
+  })
+
+  test('all() should return all the keys', async ({ assert }) => {
+    cache.set(key, value)
+    const keys = cache.all()
+    assert.deepEqual(keys, [[key, value]])
+  })
+
+  test('all() should return empty array if cache is empty', async ({ assert }) => {
+    const keys = cache.all()
+    assert.deepEqual(keys, [])
+  })
 })
